34fd657d3f8b246a68a527fdde2196c6a2c0baa2
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2018-08-13 21:02:08 +0200

    Enable use of START_BLOCK_NUM env to set a starting point for synchronization

--- pusher/src/StaticConfig.ts
+++ pusher/src/StaticConfig.ts
@@ -2,5 +2,6 @@ import { SteemOperationNumber } from "steem-wise-core";
 
 export class StaticConfig {
     public static INTRODUCTION_OF_SMARTVOTES_MOMENT: SteemOperationNumber = new SteemOperationNumber(21622860, 26, 0);
+    public static START_BLOCK_NUM: number = process.env.START_BLOCK_NUM ? parseInt(process.env.START_BLOCK_NUM as string) : StaticConfig.INTRODUCTION_OF_SMARTVOTES_MOMENT.blockNum;
     public static TIMEOUT_MS: number = 9000;
 }
