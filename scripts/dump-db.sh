2f6f7cb1e93bada452fdaa4a8e7f1e2bf494e80a
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2018-08-14 10:46:46 +0200

    Add dump-db sh script

---
+++ scripts/dump-db.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+##
+# This script dumps SQL from inside a running container.
+##
+set -x
+set -e
+
+mkdir -p backup
+docker exec steem-wise-sql_db_1 bash -c "PGPASSWORD=\"postgres\" pg_dump --username=postgres app_db" > "backup/steem-wise-sql-dump-$(date +"%Y_%m_%d_%I_%M_%p").sql"
