15fa67b766fd822ef69ffc8b9418865c9af6c152
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2018-10-02 20:12:47 +0200

    ci: add preprocess script

---
+++ .preprocess.ts
@@ -0,0 +1,9 @@
+// this file is loaded by wise-ci preprocessor
+
+export const hooks = [];
+export const data = {
+    repository: {
+        name: "steem-wise-sql"
+    }
+};
+export const excludes = [ "dist", ".vscode", "coverage" ];
