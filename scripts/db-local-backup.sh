b3e722c901ad525bbcabf7871b4a2049f229c800
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2018-10-27 12:21:37 +0200

    feat: local db backup script

--- scripts/db-local-backup.sh
+++ scripts/db-local-backup.sh
@@ -5,7 +5,12 @@
 set -x
 set -e
 
+DATED_FILEPATH="/backup/wise/sql/wise-sql-dump-$(date +"%Y_%m_%d_%I_%M_%p").sql"
+echo "${DATED_FILEPATH}"
+
 mkdir -p /backup/wise/sql/
+
 #§ 'docker exec ' + data.config.sql.docker.services.db.container + ' \\'
 docker exec wise_sql_db \
-     bash -c "PGPASSWORD=\"postgres\" pg_dump --username=postgres app_db" > "/backup/wise/sql/steem-wise-sql-dump-$(date +"%Y_%m_%d_%I_%M_%p").sql"
+     bash -c "PGPASSWORD=\"postgres\" pg_dump --username=postgres app_db" > "${DATED_FILEPATH}"
+
